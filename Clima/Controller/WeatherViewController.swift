9ea894a5233483be9c4ad145cf028fa9035d2977
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-20 00:20:15 +0530

    created the deligate as well as the 'return' key press method provided by the uitext filed

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -2,23 +2,36 @@
 //  ViewController.swift
 //  Clima
 //
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
+
+
 //
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+        
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        print(searchTextField.text!)
+    }
+    
+//    we are using the below func as if we have created the ib action on return button because when we press the return button on keyboard we are prosessing the code written between the curly braces.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        return true
+    }
+    
 }
 
