81a23d63602ec81fbe5cf40a56a813f92ce8f1d8
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-21 20:25:11 +0530

    implimented the dispatch queue for updating the labels in our interface

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -68,7 +68,12 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     }
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel){
-        print(weather.temperature)
+        
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+        
     }
     
     func didFailWithError(error: any Error) {
